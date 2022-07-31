6ec1b00e05293749792994dc9edc16c94df97dd9
Ilya Babanov <ilbabanov@gmail.com>
2022-07-31 11:31:18 +0400

    add action in form

--- app/routes/games/new.tsx
+++ app/routes/games/new.tsx
@@ -1,3 +1,20 @@
+import { ActionFunction, redirect } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+
+export const action: ActionFunction =async ({ request }) => {
+  const form = await request.formData();
+  const name = form.get('name');
+  const content = form.get('content');
+  if (typeof name !== 'string' || typeof content !== 'string') {
+    throw new Error('Form not sibmitted correctly');
+  }
+
+  const fields = {name, content};
+
+  const game = await db.game.create({data: fields});
+  return redirect(`/games/${game.id}`);
+}
+
 export default function NewGameRoute() {
   return (
     <div>
