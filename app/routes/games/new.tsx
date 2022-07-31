78ff9f310b0252b284766b74ea84274ac5c2a855
Ilya Babanov <ilbabanov@gmail.com>
2022-07-31 12:23:32 +0400

    add SS validation

--- app/routes/games/new.tsx
+++ app/routes/games/new.tsx
@@ -1,36 +1,118 @@
-import { ActionFunction, redirect } from "@remix-run/node";
+import { ActionFunction, json } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
 
-export const action: ActionFunction =async ({ request }) => {
-  const form = await request.formData();
-  const name = form.get('name');
-  const content = form.get('content');
-  if (typeof name !== 'string' || typeof content !== 'string') {
-    throw new Error('Form not sibmitted correctly');
+function validateGameContent(content: string) {
+  if (content.length < 10) {
+    return "This game is too short";
   }
-
-  const fields = {name, content};
-
-  const game = await db.game.create({data: fields});
-  return redirect(`/games/${game.id}`);
 }
 
+function validateGameName(name: string) {
+  if (name.length < 3) {
+    return "That game's name is too short";
+  }
+}
+
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    name: string | undefined;
+    content: string | undefined;
+  };
+  fields?: {
+    name: string;
+    content: string;
+  };
+};
+
+const badRequest = (data: ActionData) => json(data, { status: 400 });
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const name = form.get("name");
+  const content = form.get("content");
+  if (typeof name !== "string" || typeof content !== "string") {
+    return badRequest({
+      formError: "Form not sibmitted correctly",
+    });
+  }
+
+  const fieldErrors = {
+    name: validateGameName(name),
+    content: validateGameContent(content),
+  };
+
+  const fields = { name, content };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({
+      fieldErrors,
+      fields,
+    });
+  }
+
+  const game = await db.game.create({ data: fields });
+  return redirect(`/games/${game.id}`);
+};
+
 export default function NewGameRoute() {
+  const actionData = useActionData<ActionData>();
+
   return (
     <div>
       <p>Create a new Game</p>
       <form method="post">
         <div>
           <label>
-            Name: <input type="text" name="name" />
+            Name:{" "}
+            <input
+              type="text"
+              name="name"
+              defaultValue={actionData?.fields?.name}
+              aria-invalid={Boolean(actionData?.fieldErrors?.name) || undefined}
+              aria-errormessage={
+                actionData?.fieldErrors?.name ? "name-error" : undefined
+              }
+            />
           </label>
+          {actionData?.fieldErrors?.name ? (
+            <p className="form-validation-error" role="alert" id="name-error">
+              {actionData.fieldErrors.name}
+            </p>
+          ) : null}
         </div>
         <div>
           <label>
-            Content: <textarea name="content" />
+            Content:{" "}
+            <textarea
+              name="content"
+              defaultValue={actionData?.fields?.content}
+              aria-invalid={
+                Boolean(actionData?.fieldErrors?.content) || undefined
+              }
+              aria-errormessage={
+                actionData?.fieldErrors?.content ? "content-error" : undefined
+              }
+            />
           </label>
+          {actionData?.fieldErrors?.content ? (
+            <p
+              className="form-validation-error"
+              role="alert"
+              id="content-error"
+            >
+              {actionData.fieldErrors.content}
+            </p>
+          ) : null}
         </div>
         <div>
+          {actionData?.formError ? (
+            <p className="form-validation-error" role="alert">
+              {actionData.formError}
+            </p>
+          ) : null}
           <button type="submit" className="button">
             Add
           </button>
