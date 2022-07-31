f78190dcd9252aa5bff1c85c154b481a78dd7ed2
Ilya Babanov <ilbabanov@gmail.com>
2022-07-31 11:04:42 +0400

    add db connection

--- app/routes/games/$gameId.tsx
+++ app/routes/games/$gameId.tsx
@@ -1,8 +1,32 @@
+import type { Game } from "@prisma/client"
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { game: Game };
+
+export const loader: LoaderFunction = async ({ params }) => {
+    const game = await db.game.findUnique({
+        where: { id: params.gameId },
+    });
+
+    if (!game) {
+        throw new Error("Game not found");
+    }
+
+    const data: LoaderData = { game }
+
+    return json(data);
+};
+
 export default function GameRoute() {
+    const data = useLoaderData<LoaderData>();
     return (
         <div>
             <p>It's one of games</p>
-            <p>Results of this game</p>
+            <p>{data.game.content}</p>
+            <Link to='.'>{data.game.name} Permalink</Link>
         </div>
     )
 }
--- app/routes/games/index.tsx
+++ app/routes/games/index.tsx
@@ -1,8 +1,32 @@
+import type { Game } from "@prisma/client";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { randomGame: Game };
+
+export const loader: LoaderFunction = async () => {
+  const count = await db.game.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomGame] = await db.game.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+  const data: LoaderData = { randomGame };
+  return json(data);
+};
+
 export default function GamesIndexRoute() {
-    return (
-        <div>
-            <p>It's last game</p>
-            <p>Last game results</p>
-        </div>
-    );
+  const data = useLoaderData<LoaderData>();
+
+  return (
+    <div>
+      <p>It's random Game:</p>
+      <p>{data.randomGame.content}</p>
+      <Link
+        to={data.randomGame.id}
+      >{`"${data.randomGame.name}" Permalink`}</Link>{" "}
+    </div>
+  );
 }
