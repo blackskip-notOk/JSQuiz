72b0665f2065aed56200b6be79b595628e1a42d3
Ilya Babanov <ilbabanov@gmail.com>
2022-08-06 20:23:37 +0400

    add rules

--- app/routes/rules.tsx
+++ app/routes/rules.tsx
@@ -5,26 +5,47 @@ import { links as HeaderNavLinks } from '~/components/headerNav';
 import { links as TranslationToggleLinks } from '~/components/translationToggle';
 import { links as LogoLinks } from '~/components/logo';
 import i18next from '~/i18n/i18n.server';
+import { useTranslation } from 'react-i18next';
+import stylesUrl from '~/styles/rules.css';
 
 export const loader: LoaderFunction = async ({ request }) => {
 	const t = await i18next.getFixedT(request);
-	const title = t('header.default');
-	const description = t('description.headerDefault');
+	const title = t('header.rules');
+	const description = t('description.headerRules');
 	return json({ title, description });
 };
 
 export const links: LinksFunction = () => {
-	return [...TranslationToggleLinks(), ...HeaderNavLinks(), ...LogoLinks()];
+	return [
+		...TranslationToggleLinks(),
+		...HeaderNavLinks(),
+		...LogoLinks(),
+		{ rel: 'stylesheet', href: stylesUrl },
+	];
 };
 
 export const meta: MetaFunction = ({ data }) => {
 	return { title: data.title, description: data.description };
 };
 
+export const handle = {
+	i18n: 'rules',
+};
+
 export default function RulesRoute() {
+	const { t } = useTranslation('rules');
+
 	return (
-		<article className='content'>
+		<div className='rulesContainer'>
 			<h1>Rules</h1>
-		</article>
+			<ul>
+				<li>{t('first')}</li>
+				<li>{t('second')}</li>
+				<li>{t('third')}</li>
+				<li>{t('forth')}</li>
+				<li>{t('fifth')}</li>
+				<li>{t('sixth')}</li>
+			</ul>
+		</div>
 	);
 }
