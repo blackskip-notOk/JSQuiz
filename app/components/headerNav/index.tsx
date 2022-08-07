6328b1fef0b2047cc0633f865d292416ed143eaa
Ilya Babanov <ilbabanov@gmail.com>
2022-08-08 00:17:08 +0400

    add login styles

--- app/components/headerNav/index.tsx
+++ app/components/headerNav/index.tsx
@@ -1,6 +1,6 @@
-import { Link } from "@remix-run/react";
-import { useTranslation } from "react-i18next";
-import { Route } from "~/constants";
+import { NavLink } from '@remix-run/react';
+import { useTranslation } from 'react-i18next';
+import { Route } from '~/constants';
 import stylesUrl from './style.css';
 
 export function links() {
@@ -12,18 +12,36 @@ export const handle = {
 };
 
 export const HeaderNav = () => {
-    const { t } = useTranslation('header');
+	const { t } = useTranslation('header');
 	return (
 		<nav>
-			<ul className="linksList">
-				<li className="linkItem">
-					<Link to={Route.rules} prefetch='intent' className="link">{t('link.rules')}</Link>
+			<ul className='linksList'>
+				<li className='linkItem'>
+					<NavLink
+						to={Route.rules}
+						prefetch='intent'
+						className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+					>
+						{t('link.rules')}
+					</NavLink>
 				</li>
-				<li className="linkItem">
-					<Link to={Route.games} prefetch='intent' className="link">{t('link.results')}</Link>
+				<li className='linkItem'>
+					<NavLink
+						to={Route.games}
+						prefetch='intent'
+						className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+					>
+						{t('link.results')}
+					</NavLink>
 				</li>
-				<li className="linkItem">
-					<Link to={Route.game} prefetch='intent' className="link">{t('link.newGame')}</Link>
+				<li className='linkItem'>
+					<NavLink
+						to={Route.game}
+						prefetch='intent'
+						className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+					>
+						{t('link.newGame')}
+					</NavLink>
 				</li>
 			</ul>
 		</nav>
